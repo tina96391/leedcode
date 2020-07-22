9ce9cd43298486f7bdaff21f785edce1d9d4ebdc
Dai yu-ting <tina963912@gmail.com>
2020-07-22 09:32:49 +0800

    Update 1342_Number of Steps to Reduce a Number to Zero.py

--- Easy/1342_Number of Steps to Reduce a Number to Zero.py
+++ Easy/1342_Number of Steps to Reduce a Number to Zero.py
@@ -8,3 +8,7 @@ class Solution(object):
                 num=num-1
             count=count+1
         return count
+"""
+Runtime: 32 ms, faster than 11.90% of Python online submissions for Number of Steps to Reduce a Number to Zero.
+Memory Usage: 12.7 MB, less than 75.99% of Python online submissions for Number of Steps to Reduce a Number to Zero.
+"""
