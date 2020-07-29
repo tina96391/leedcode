cb2a1fb644815d82fd7448f1fbab26706f93725a
Dai yu-ting <tina963912@gmail.com>
2020-07-29 10:12:16 +0800

    Update 1528_Shuffle String.py

--- Easy/1528_Shuffle String.py
+++ Easy/1528_Shuffle String.py
@@ -12,6 +12,8 @@ class Solution(object):
             wordlist[si] = s[i]
         return "".join(wordlist)
 """
+利用enumerate的概念取出i及indices(si)的數字(0,4),(1,5)...etc
+重新排列wordlist後join成新詞
 Runtime: 48 ms, faster than 62.61% of Python online submissions for Shuffle String.
 Memory Usage: 12.9 MB, less than 19.14% of Python online submissions for Shuffle String.
 """
